c21eaad9c93f3beafc4ce55a04e0f122ca1c2c24
yoonsang Lee <highp0912@pm.me>
2023-10-28 21:19:38 +0900

    add: ReadWriteLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,34 +1,108 @@
-﻿
+﻿// using System.Diagnostics;
+// using ServerCore;
+//
+// Stopwatch sw = Stopwatch.StartNew();
+// EventLock _lock = new();
+// bool _bLockTaken = false;
+// SpinLock _spinLock = new();
+//
+// int num = 0;
+//
+// Task t1 = new(Thread1);
+// Task t2 = new(Thread2);
+// t1.Start();
+// t2.Start();
+//
+// Task.WaitAll(t1, t2);
+//
+// Console.WriteLine(num);
+// sw.Stop();
+//
+// Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+//
+// ReaderWriterLockSlim _rwLock = new();
+// return;
+//
+//
+// void Thread1() {
+//   for (int i = 0; i < 100_000; i++) {
+//     try {
+//       // _lock.Acquire();
+//       _spinLock.Enter(ref _bLockTaken);
+//       num++;
+//     }
+//     finally {
+//       // _lock.Release();
+//
+//       if (_bLockTaken)
+//         _spinLock.Exit();
+//     }
+//   }
+// }
+//
+// void Thread2() {
+//   for (int i = 0; i < 100_000; i++) {
+//     try {
+//       // _lock.Acquire();
+//       _spinLock.Enter(ref _bLockTaken);
+//       num--;
+//     }
+//     finally {
+//       // _lock.Release();
+//       if (_bLockTaken)
+//         _spinLock.Exit();
+//     }
+//   }
+// }
+
+// Reward GetRewardById(int id) {
+//   try {
+//     _rwLock.EnterReadLock();
+//
+//     return null;
+//   }
+//   finally {
+//     _rwLock.ExitReadLock();
+//   }
+// }
+//
+// void AddReward(Reward reward) {
+//   try {
+//     _rwLock.EnterWriteLock();
+//   }
+//   finally {
+//     _rwLock.ExitWriteLock();
+//   }
+// }
+//
+// class Reward {
+// }
+
+using System.Security.Cryptography;
+using System.Threading.Channels;
 using ServerCore;
 
-MySpinLock _spinLock = new MySpinLock();
+int cnt = 0;
+RWLock _rwLock = new RWLock();
+Task t1 = new(() => {
+  for (int i = 0; i < 100_000_000; i++) {
+    _rwLock.WriteLock();
+    cnt++;
+    _rwLock.WriteUnlock();
+  }
+});
+Task t2 = new(() => {
+  for (int i = 0; i < 100_000_000; i++) {
+    _rwLock.WriteLock();
+    _rwLock.WriteLock();
+    cnt--;
+    _rwLock.WriteUnlock();
+    _rwLock.WriteUnlock();
+  }
+});
 
-int num = 0;
-
-Task t1 = new(Thread1);
-Task t2 = new(Thread2);
 t1.Start();
 t2.Start();
-
 Task.WaitAll(t1, t2);
 
-Console.WriteLine($"t1: {t1}, t2: {t2}, same? {t1 == t2}");
-void Thread1()
-{
-    for (int i = 0; i < 100_000; i++)
-    {
-        _spinLock.Acquire();
-        num++;
-        _spinLock.Release();
-    }
-}
-
-void Thread2()
-{
-    for (int i = 0; i < 100_000; i++)
-    {
-        _spinLock.Acquire();
-        num--;
-        _spinLock.Release();
-    }
-}
+Console.Write(cnt);
--- ServerCore/RWLock.cs
+++ ServerCore/RWLock.cs
@@ -1,6 +1,77 @@
 ﻿namespace ServerCore;
 
-public class RWLock
-{
-    
+// 1. recursive-able?
+// Write -> Write (o), Write -> Read (o), Read -> Write (x)
+// 2. spinLock policy -> 5000 spin <-> yield
+public class RWLock {
+  private const int EMPTY_FLAG = 0x00000000;
+
+  // 7 -> 0 1 1 1 -> 2^2 + 2^1 + 2^0
+  private const int WRITE_MASK = 0x7FFF0000;
+  private const int READ_MASK = 0x0000FFFF;
+  private const int MAX_SPING_COUNT = 5000;
+
+  // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
+  private int _flag = EMPTY_FLAG;
+  private int _writeCount = 0;
+
+  public void WriteLock() {
+    // check the same thread has already acquired WriteLock.
+    int lockThreadId = (_flag & WRITE_MASK) >> 16;
+    if (lockThreadId == Thread.CurrentThread.ManagedThreadId) {
+      _writeCount++;
+      return;
+    }
+
+    int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+    const int expected = EMPTY_FLAG;
+
+    // acquire after competing when no one acquires writeLock or readLock.
+    while (true) {
+      for (int i = 0; i < MAX_SPING_COUNT; ++i) {
+        // try enter and break on success;
+        if (Interlocked.CompareExchange(ref _flag, desired, expected) == EMPTY_FLAG) {
+          _writeCount += 1;
+          return;
+        }
+
+        Thread.Yield();
+      }
+    }
+  }
+
+  public void WriteUnlock() {
+    int lockCount = --_writeCount;
+    if (lockCount == 0)
+      Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+  }
+
+  public void ReadLock() {
+    int lockThreadId = (_flag & WRITE_MASK) >> 16;
+    if (Thread.CurrentThread.ManagedThreadId == lockThreadId) {
+      Interlocked.Increment(ref _flag);
+      return;
+    }
+
+    // increase 1 when no one acquires WriteLock.
+    while (true) {
+      for (int i = 0; i < MAX_SPING_COUNT; i++) {
+        int expected = _flag & READ_MASK;
+        int desired = expected + 1;
+        if (Interlocked.CompareExchange(ref _flag, desired, expected) == expected)
+          return;
+
+        // if ((_flag & WRITE_MASK) == EMPTY_FLAG) {
+        //   _flag += 1;
+        //   return;
+        // }
+      }
+
+      Thread.Yield();
+    }
+  }
+
+  public void ReadUnlock() {
+    Interlocked.Decrement(ref _flag);
+  }
 }
