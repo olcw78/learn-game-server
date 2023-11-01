50d48fc119042541f059c1846325c2eb79d62f18
yoonsang Lee <highp0912@pm.me>
2023-11-02 00:58:33 +0900

    comment: socket accept 주석

--- src/ServerCore/Socket/Listener.cs
+++ src/ServerCore/Socket/Listener.cs
@@ -30,6 +30,9 @@ public class Listener {
         _socket.Listen(backlog: DEFAULT_BACKLOG);
 
         SocketAsyncEventArgs args = new();
+        
+        // ThreadPool 에서 I/O Thread 를 하나 가져와서 Completed Event 를 호출해준다.
+        // if pending == true:
         args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
         RegisterAccept(args);
     }
@@ -40,12 +43,29 @@ public class Listener {
         args.AcceptSocket = null;
 
         bool bPending = _socket.AcceptAsync(args);
+        
+        // ThreadPool 갈 필요 없이 바로 실행.
         if (!bPending) {
             OnAcceptCompleted(null, args);
         }
     }
 
     void OnAcceptCompleted(object sender, SocketAsyncEventArgs args) {
+        Console.WriteLine($"Client Connected! pending");
+
+        if (args.SocketError == SocketError.Success) {
+            _onAcceptHandler.Invoke(args.AcceptSocket);
+        }
+        else {
+            Console.WriteLine(args.SocketError.ToString());
+        }
+
+        RegisterAccept(args);
+    }
+
+    void OnAcceptCompleted_NoPending(object sender, SocketAsyncEventArgs args) {
+        Console.WriteLine($"Client Connected! no pending");
+
         if (args.SocketError == SocketError.Success) {
             _onAcceptHandler.Invoke(args.AcceptSocket);
         }
